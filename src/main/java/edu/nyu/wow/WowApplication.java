e281e9f2c9f2cbbff251d8ac37e98c0fc7494d28
stonywu-1663686 <stonywu@uw.edu>
2021-09-16 18:10:05 -0400

    create the framework

---
+++ src/main/java/edu/nyu/wow/WowApplication.java
@@ -0,0 +1,42 @@
+package edu.nyu.wow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableAsync
+@EnableTransactionManagement
+@EnableSwagger2
+public class WowApplication {
+	public static final Logger log = LoggerFactory.getLogger(WowApplication.class);
+	public static void main(String[] args) throws UnknownHostException {
+		ApplicationContext ctx = SpringApplication.run(WowApplication.class, args);
+		Environment env = ctx.getEnvironment();
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t"
+						+ "Local: \t\thttp://localhost:{}\n\t"
+						+ "External: \thttp://{}:{}\n\t"
+						+ "Swagger : \thttp://localhost:{}/swagger-ui.html#/\n\t",
+				env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				//InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
+	}
+
+}
---
+++ src/main/java/edu/nyu/wow/ibo/PatientIbo.java
@@ -0,0 +1,9 @@
+package edu.nyu.wow.ibo;
+
+import lombok.Data;
+
+@Data
+public class PatientIbo {
+
+    private String name;
+}
